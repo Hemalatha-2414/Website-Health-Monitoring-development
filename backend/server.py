308a02c43fa4b7d201ecc8fcb0c65bc96b7ca1b9
Hemalatha-2414 <hema241410@gmail.com>
2026-08-30 19:00:01 +0400

    adding backend and frontend

--- backend/server.py
+++ backend/server.py
@@ -82,6 +82,28 @@ class WebsiteMonitorHandler(
                 404
             )
 
+    def do_OPTIONS(self):
+        self.send_response(200)
+
+        self.send_header(
+            "Access-Control-Allow-Origin",
+        "*"
+        )
+
+        self.send_header(
+            "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS"
+        )
+
+        self.send_header(
+            "Access-Control-Allow-Headers",
+        "Content-Type"
+        )
+
+        self.end_headers()
+
+
+    
 
     def do_POST(self):
 
